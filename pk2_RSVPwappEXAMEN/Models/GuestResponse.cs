44fed7171dad4d87389d31002630383d97eb9f7a
Carlos47197 <carlos_alfonso_e@hotmail.com>
2014-03-03 07:24:37 -0600

    inicio

---
+++ pk2_RSVPwappEXAMEN/Models/GuestResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pk2_RSVPwapp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace pk2_RSVPwapp.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Por favor ingrese su nombre.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="Por favor ingrese su correo electronico.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage="Por favor ingrese un correo valido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Por favor ingrese su telefono.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage="Por favor especifique si asistira o no.")]
+        public bool? WillAttend { get; set; }
+    
+    }
+
+}
---
+++ pk2_RSVPwapp_EXAMEN/pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pk2_RSVPwapp.Models;
+
+namespace pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Buenos Dias!!!" : "Buenas Tardes!!!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //Verificando errores de validacion
+            if (ModelState.IsValid)
+            {
+                //TODO: enviar respuesta al correo del organizador
+                return View("Agradecimientos", guestResponse);
+            }
+            else
+            {
+                // Hay un problema de validacion
+                return View();
+            }
+
+        }
+
+    }
+}
